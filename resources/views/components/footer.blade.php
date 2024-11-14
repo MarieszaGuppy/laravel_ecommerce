1c1c71889fbb1f3758e2663fd8a0ebecbaeed4cc
NaufalTA <Naufallopayy25@gmail.com>
2024-11-14 11:13:26 +0700

    home page baru

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,64 +1,66 @@
 <div class="container">
     <footer class="pt-5">
-        <div class="row">
+        <div class="row px-3">
             <div class="col-6 col-md-2 mb-3">
-                <h5 class="text-white">Toko</h5>
+                <h4 class="text-white">Toko</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Lokasi</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Tentang Kami</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Hubungi Kami</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Lokasi</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Tentang Kami</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Hubungi Kami</a></li>
                 </ul>
             </div>
 
             <div class="col-6 col-md-2 mb-3">
-                <h5 class="text-white">Bantuan</h5>
+                <h4 class="text-white">Bantuan</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">FAQ</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Pembayaran</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Kebijakan Privasi</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQ</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pembayaran</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Kebijakan Privasi</a></li>
                 </ul>
             </div>
 
             <div class="col-6 col-md-2 mb-5">
-                <h5 class="text-white">Costumer</h5>
+                <h4 class="text-white">Costumer</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Lacak Pesanan</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Lacak Pesanan</a></li>
                 </ul>
             </div>
 
             <div class="col-6 col-md-2 mb-5">
-                <h5 class="text-white">Produk</h5>
+                <h4 class="text-white">Produk</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Sale</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Koleksi Baru</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Kaos</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Hoodie</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Topi</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white-50">Aksesoris</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Sale</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Koleksi Baru</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Kaos</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Hoodie</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Topi</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Aksesoris</a></li>
                 </ul>
             </div>
 
             <div class="col-md-4 mb-3">
                 <form>
+                <div class="py-4">
+                    <h1 class="fw-bold text-white m-0">ICONLOGO</h1>
+                    <div class="d-flex align-items-center text-white gap-1">
+                        <a href="text-decoration-none"><i class='bx bxl-twitter fs-5 text-white' ></i></a>
+                        <a href="text-decoration-none"><i class='bx bxl-facebook fs-5 text-white' ></i></a>
+                        <a href="text-decoration-none"><i class='bx bxl-instagram fs-5 text-white' ></i></a>
+                        <a href="text-decoration-none"><i class='bx bxl-whatsapp fs-5 text-white'></i></a>
+                    </div>
+                </div>
                     <h5 class="text-white">Subscribe to our newsletter</h5>
-                    <p class="text-white">Monthly digest of what's new and exciting from us.</p>
                     <div class="d-flex flex-column flex-sm-row w-100 gap-2">
                         <label for="newsletter1" class="visually-hidden">Email address</label>
                         <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
                         <button class="btn btn-outline-light" type="button">Subscribe</button>
                     </div>
                 </form>
-                
             </div>
         </div>
 
-        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 mt-4 border-top">
+        <div class="d-flex justify-content-center py-3 mt-4 border-top">
             <p class="text-white">&copy; 2024 Company, Inc. All rights reserved.</p>
-            <ul class="list-unstyled d-flex">
-                <li class="ms-3"><a class="text-white" href="#"><i class='bx bxl-twitter' ></i></a></li>
-                <li class="ms-3"><a class="text-white" href="#"><i class='bx bxl-facebook-circle' ></i></a></li>
-                <li class="ms-3"><a class="text-white" href="#"><i class='bx bxl-instagram' ></i></a></li>
-            </ul>
         </div>
     </footer>
 </div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,207 +1,188 @@
 @extends('layouts.main')
 
+@section('header')
+    <style>
+        .heros {
+            height: 30rem;
+        }
+
+        @media(max-width: 900px) {}
+
+        @media(max-width: 768px) {
+            .gap-heros {
+                margin-bottom: 1rem;
+            }
+
+            .heros {
+                height: 20rem;
+            }
+        }
+
+        @media(max-width: 500px) {}
+    </style>
+@endsection
+
 @section('container')
-    <div class="container col-xxl-10 px-4 py-5">
-
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 align-items-center flex-lg-row-reverse g-5 py-5"
-            style="margin-bottom: 5rem;">
-            <div class="col d-flex justify-content-center">
-                <div class="d-flex justify-content-center align-items-center"
-                    style="border-radius: 0px 0px 0px 80px; width: 30rem; height: 30rem; background-color: #009785;">
-                    <img src="https://picsum.photos/200/400?random=1" class="d-block mx-lg-auto img-fluid p-4"
-                        alt="Bootstrap Themes" loading="lazy">
-                </div>
-            </div>
-            <div class="col">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
-                    most
-                    popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-                    extensive prebuilt components, and powerful JavaScript plugins.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn px-5 py-2 me-md-2 text-white" style="background-color: #009785">Order
-                        Now!</button>
-                </div>
-            </div>
-        </div>
-
-        <div style="margin-bottom: 5rem;">
-            <h1 class="text-center py-3">New Collections!</h1>
-            <div class="row row-cols-1 row-cols-lg-3 row-cols-md-3">
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+    <div class="container py-5">
+        <div class="row my-3">
+            <div class="col-lg-8 col-md-7">
+                <div class="card gap-heros heros">
+                    <img src="{{ asset('storage/images/homepage/heros/NeverRegretClothes.jpeg') }}" class="card-img img-fluid"
+                        alt="banner" style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                    <div class="card-img-overlay d-flex align-items-center text-white"
+                        style="background-color: rgba(0, 0, 0, 0.45)">
+                        <div class="p-3">
+                            <h1 class="m-0 fw-bold">With Great Design <br> <span style="color: #53ff5f">Make You
+                                    Perfect!</span></h1>
+                            <p class="card-text m-0 py-4"><small>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Non molestiae ex cumque repellat sint exercitationem dicta ratione aut omnis
+                                    veritatis.</small></p>
+                            <a href="" class="btn btn-outline-light px-4 py-2 fw-bold">Shop now!</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+            </div>
+            <div class="col-lg-4 col-md-5">
+                <div class="d-flex h-100 flex-column justify-content-between gap-3">
+                    <div class="card" style="height: 14rem;">
+                        <img src="{{ asset('storage/images/homepage/heros/smiley-man-posing-outdoors-front-gate.jpg') }}"
+                            alt="" class="img-fluid"
+                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                        <div class="position-absolute w-100 h-100 d-flex align-items-end"
+                            style="background-color: rgba(0, 0, 0, 0.5);">
+                            <div class="p-4">
+                                <h3 class="fw-bold m-0" style="color: #70ff7a;">Articles</h3>
+                                <p class="text-white"><small>Lorem ipsum dolor sit, amet consectetur adipisicing
+                                        elit.</small></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+
+                    <div class="card" style="height: 14rem;">
+                        <img src="{{ asset('storage/images/homepage/heros/Black.png') }}" alt="" class="img-fluid"
+                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                        <div class="position-absolute w-100 h-100 d-flex align-items-end"
+                            style="background-color: rgba(0, 0, 0, 0.5);">
+                            <div class="p-4">
+                                <h3 class="fw-bold m-0" style="color: #70ff7a;">Location</h3>
+                                <p class="text-white"><small>Lorem ipsum dolor sit, amet consectetur adipisicing
+                                        elit.</small></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 
-    <div class="py-5" style="margin-bottom: 5rem; background-color: #b6fff1;">
+    <div class="container mt-5">
+        <div style="margin-bottom: 5rem;">
+            <div class="text-center py-4">
+                <h1 class="m-0 fw-bold">New <span style="color: #1bac25;">Collections!</span></h1>
+                <p class="text-secondary m-0"><small>Check our new collaborations!</small></p>
+            </div>
+            <div class="row row-cols-1 row-cols-lg-3 row-cols-md-3">
+                <div class="col mb-4">
+                    <div class="card">
+                        <img src="{{ asset('storage/images/homepage/collections/Pokemon.jpeg') }}" alt=""
+                            class="img-fluid"
+                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                        <div class="position-absolute w-100 h-100 d-flex align-items-center justify-content-center"
+                            style="background-color: rgba(0, 0, 0, 0.5);">
+                            <h2 class="text-white text-center fw-bold">Toko <br> <span style="color: #1bac25;">x</span> <br>
+                                Pokemon</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+                        <img src="{{ asset('storage/images/homepage/collections/demonSlayerjpeg.jpeg') }}" alt=""
+                            class="img-fluid"
+                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                        <div class="position-absolute w-100 h-100 d-flex align-items-center justify-content-center"
+                            style="background-color: rgba(0, 0, 0, 0.5);">
+                            <h2 class="text-white text-center fw-bold">Toko <br> <span style="color: #1bac25;">x</span> <br>
+                                Demon Slayer</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="card">
+                        <img src="{{ asset('storage/images/homepage/collections/BMTH.jpeg') }}" alt=""
+                            class="img-fluid"
+                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                        <div class="position-absolute w-100 h-100 d-flex align-items-center justify-content-center"
+                            style="background-color: rgba(0, 0, 0, 0.5);">
+                            <h2 class="text-white text-center fw-bold">Toko <br> <span style="color: #1bac25;">x</span> <br>
+                                BMTH</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="" style="margin-bottom: 5rem; padding-bottom: 8rem; padding-top: 5rem; background-color: #5dff6830;">
         <div class="container">
             <div class="text-center text-black py-4">
-                <h1 class="text-center">Best Product Seller!</h1>
-                <p class="text-secondary"><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa vel,
+                <h1 class="text-center fw-bold m-0"><span style="color: #1bac25;">BEST</span> Product Sellers!</h1>
+                <p class="text-dark"><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa vel,
                         blanditiis voluptatum porro
                         corporis ipsa ipsam, quibusdam laboriosam corrupti voluptatibus omnis optio modi ut eligendi
                         adipisci sint inventore. Atque, ipsum.</small></p>
             </div>
-            <div class="row row-cols-1 row-cols-lg-5 row-cols-md-5">
+            <div class="row row-cols-2 row-cols-md-4 row-cols-lg-4">
                 <div class="col">
                     <div class="card">
+                        <img src="{{ asset('storage/images/homepage/heros/NeverRegretClothes.jpeg') }}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=2" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
+                
                 <div class="col">
                     <div class="card">
+                        <img src="{{ asset('storage/images/homepage/heros/NeverRegretClothes.jpeg') }}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=2" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
+                
                 <div class="col">
                     <div class="card">
+                        <img src="{{ asset('storage/images/homepage/heros/NeverRegretClothes.jpeg') }}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=2" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
+                
                 <div class="col">
                     <div class="card">
+                        <img src="{{ asset('storage/images/homepage/heros/NeverRegretClothes.jpeg') }}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=2" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <img src="https://picsum.photos/200/200?random=2" alt="" class="img-fluid"
-                                style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
 
-    </div>
-
-    <div class="container py-5">
-        <div class="text-center text-black py-4">
-            <h1 class="text-center">Best Product Seller!</h1>
-            <p class="text-secondary"><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa vel,
-                    blanditiis voluptatum porro
-                    corporis ipsa ipsam, quibusdam laboriosam corrupti voluptatibus omnis optio modi ut eligendi
-                    adipisci sint inventore. Atque, ipsum.</small></p>
-        </div>
-        <div class="row row-cols-1 row-cols-lg-5 row-cols-md-5">
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="https://picsum.photos/200/200?random=1" alt="" class="img-fluid"
-                            style="object-fit: cover; object-position: center; width: 100%; height: 100%;">
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
 
